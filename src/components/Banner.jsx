78278b6f5ce518bfa53ff8e135e031307ddd78fc
Krishna Prasad <krishnaprasadsm63gmail.com@Krishnas-MacBook-Air.local>
2025-08-09 02:41:16 +0530

    banner change

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import '../styles/Banner.css'
+import '../styles/Banner.css';
+import awsLogo from '/images/aws.svg';
+import notionLogo from '/images/notion.svg';
+import bubbleLogo from '/images/bubble.svg';
+import mongodbLogo from '/images/mongo.svg';
 
 function Banner() {
     return (
         <section className="banner">
-            <div className="content1">
-                <p>Employees</p>
-                <h1>5+</h1>
-            </div>
-            <div className="content1">
-                <p>Founded</p>
-                <h1>2025</h1>
-            </div>
-            <div className="content1">
-                <p>Product in Development</p>
-                <h1>1+</h1>
-            </div>
-            <div className="content1">
-                <p>Incubated At</p>
-                <h1>MBCET</h1>
+            <h3 className="banner-heading">Powered by</h3>
+            <div className="logo-container">
+                <img src={awsLogo} alt="AWS" className="logo aws-logo" />
+                <img src={notionLogo} alt="Notion" className="logo notion-logo" />
+                <img src={bubbleLogo} alt=".bubble" className="logo bubble-logo" />
+                <img src={mongodbLogo} alt="MongoDB for Startups" className="logo mongodb-logo" />
             </div>
         </section>
     );
