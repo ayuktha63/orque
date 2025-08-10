9aadb26451ad726b92c1aa8293ce15ec562a9771
rizzwaaaan <145579575+rizzwaaaan@users.noreply.github.com>
2025-08-10 11:04:13 +0530

    test

--- src/components/ContactBanner.jsx
+++ src/components/ContactBanner.jsx
@@ -11,7 +11,7 @@ function ContactBanner() {
                     <HiOutlineSpeakerphone size={18} />
                     <span>We are available 24 Hours</span>
                 </div>
-                <h2>Have a Project in Mind? Let’s Talk</h2>
+                <h2>Havee a Project in Mind? Let’s Talk</h2>
                 <p>
                     We design and develop custom software, intelligent AI systems, and modern websites that help businesses scale and innovate. Whether you're starting from scratch or improving an existing solution, we’ll craft a tailored approach to bring your vision to life.
                 </p>
